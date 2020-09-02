039a80c72cdc2223fb52cd72f051ca5d779ccdf7
LCL <44160792+LCLDL1999@users.noreply.github.com>
2020-09-02 18:48:29 +0800

    Testing 1

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Hello />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Hello from './components/Hello.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Hello
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
